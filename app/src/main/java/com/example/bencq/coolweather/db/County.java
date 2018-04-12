2e87313810f1266d009cb129b008d00d301d8de2
Bencq <894056215@qq.com>
2018-04-12 16:44:23 +0800

    Second commit.

---
+++ app/src/main/java/com/example/bencq/coolweather/db/County.java
@@ -0,0 +1,46 @@
+package com.example.bencq.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Bencq on 2018/04/12.
+ */
+
+public class County extends DataSupport{
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
